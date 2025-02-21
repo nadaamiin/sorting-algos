1a8154d99a1fd9723b89b43e8724c824bf55fc22
nada <nadaamiin00@gmail.com>
2025-02-21 19:33:41 +0200

    creating main

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to SortMaster\n--------------------------");
+        System.out.println("Do you want to\n1-Enter your array\n2-Generate an array");
     }
 }
+
+
